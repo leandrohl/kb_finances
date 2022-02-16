25a3c8939a3869923c6eb7bd7f014d80ac36ae4a
Leandro <leandro.silva@marttech.com.br>
2022-02-16 11:39:05 -0300

    removendo header

--- src/services/axios.tsx
+++ src/services/axios.tsx
@@ -1,18 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://kbfinances.herokuapp.com/',
-  headers: {
-    origin: document.location.origin
-  }
+  baseURL: 'https://kbfinances.herokuapp.com/'
 })
-
-// apiConfig.interceptors.request.use(async (config) => {
-//   const token = getToken();
-
-//   if (token) config.headers.Authorization = `Bearer ${token}`;
-
-//   return config;
-// });
 
 export default api
